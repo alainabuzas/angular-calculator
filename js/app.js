c50245902b94eabf2d82e00547d5c59cea8cf01f
srobertson421 <srobertson421@gmail.com>
2017-01-17 16:27:13 -0800

    eval solution

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log('app.js was loaded.');
+angular.module('CalcApp', [])
+.controller('CalcCtrl', ['$scope', function($scope) {
+  var calcEntries = '';
+
+  $scope.calcVal = '0';
+
+  $scope.buttonClick = function(symbolNum) {
+    var lastEntry = calcEntries[calcEntries.length - 1];
+
+    if($scope.calcVal === '0') {
+      $scope.calcVal = symbolNum.toString();
+      calcEntries += symbolNum.toString();
+    } else if(/[+-/*]/.test(symbolNum)) {
+      $scope.calcVal = eval(calcEntries);
+      calcEntries += symbolNum;
+    } else {
+      if(/[+-/*]/.test(lastEntry)) {
+        $scope.calcVal = symbolNum.toString();
+      } else {
+        $scope.calcVal += symbolNum.toString();
+      }
+      calcEntries += symbolNum.toString();
+    }
+  }
+
+  $scope.clear = function() {
+    $scope.calcVal = '0';
+    calcEntries = '';
+  }
+
+  $scope.evaluate = function() {
+    $scope.calcVal = eval(calcEntries);
+  }
+}]);
